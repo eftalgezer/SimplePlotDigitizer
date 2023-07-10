f08e3fc7931c46685f2054c28a95f536239c3036
Eftal Gezer <eftalgezer@gmail.com>
2023-07-11 00:51:51 +0300

    Update core.py

--- SimplePlotDigitizer/core.py
+++ SimplePlotDigitizer/core.py
@@ -188,38 +188,3 @@ def run(args):
         for r in traj:
             f.write("%g %g\n" % (r))
     logging.info(f"Wrote trajectory to {outfile}")
-
-
-def main():
-    # Argument parser.
-    import argparse
-
-    description = """Digitize image."""
-    parser = argparse.ArgumentParser(description=description)
-    parser.add_argument("INPUT", type=Path, help="Input image file.")
-    parser.add_argument(
-        "--plot",
-        default=None,
-        required=False,
-        help="Plot the final result. Requires matplotlib.",
-    )
-    parser.add_argument(
-        "--output",
-        "-o",
-        required=False,
-        type=str,
-        help="Name of the output file else trajectory will be written to "
-        " <INPUT>.traj.csv",
-    )
-    parser.add_argument(
-        "--preprocess",
-        required=False,
-        action="store_true",
-        help="Preprocess the image. Useful with bad resolution images.",
-    )
-    args = parser.parse_args()
-    run(args)
-
-
-if __name__ == "__main__":
-    main()
