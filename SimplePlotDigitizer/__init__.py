f722ff8547c2dc4e92c2d87240a408ff026fabcc
Eftal Gezer <eftalgezer@gmail.com>
2023-07-11 01:19:27 +0300

    Update __init__.py

--- SimplePlotDigitizer/__init__.py
+++ SimplePlotDigitizer/__init__.py
@@ -1,1 +1,12 @@
+"""
+A Python3 command line utility to digitize plots in batch mode.
+"""
 
+from __future__ import absolute_import
+
+# meta
+__title__ = "SimplePlotDigitizer"
+__author__ = "Eftal Gezer"
+__license__ = "GNU GPL v3"
+__copyright__ = "Copyright 2023, Eftal Gezer"
+__version__ = "0.1.0"
