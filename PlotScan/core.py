87510096aa5545065f915f32f1ad492c1b8f9af1
Eftal Gezer <eftalgezer@gmail.com>
2023-07-28 19:48:12 +0300

    Update core.py

--- PlotScan/core.py
+++ PlotScan/core.py
@@ -169,8 +169,8 @@ def run(args):
     assert img_.max() <= 255
     assert img_.min() < img_.mean() < img_.max(), "Could not read meaningful data"
     points = find_points(infile)
-    points_ = list_to_points(str[point[1] for point in points].strip("[").strip("]"))
-    locations_ = list_to_points(str[point[0] for point in points].strip("[").strip("]"))
+    points_ = list_to_points(str([point[1] for point in points]).replace("[", "").replace("]", ""))
+    locations_ = list_to_points(str([point[0] for point in points]).replace("[", "").replace("]", ""))
     #logging.debug(f"data points {args.data_point} → location on image {args.location}")
 
     traj = process_image(img_)
