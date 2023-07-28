f2bbbadcb53a149f9f3346729d163eb41bc3c504
Eftal Gezer <eftalgezer@gmail.com>
2023-07-28 22:02:13 +0300

    Update test_cli.py

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -6,8 +6,14 @@ import PlotScan
 HERE = Path(__file__).parent.resolve()
 
 
-def _run_cmdline(infile: Path,):
+def _run_cmdline(infile: Path, points=None, locations=None):
     cmd = f"PlotScan {str(infile)} "
+    if points:
+        pts = " ".join([f"-p {','.join(map(str,pt))}" for pt in points])
+    if locations:
+        locs = " ".join([f"-l {','.join(map(str,pt))}" for pt in locations])
+    if points and locations:
+        cmd += f"{pts} {locs}"
     outfile = infile.with_suffix(".result.png")
     trajfile = infile.with_suffix(".result.csv")
     cmd += f" --plot {str(outfile)} --output {trajfile}"
@@ -30,18 +36,22 @@ def test_trimmeed():
     _check_csv_file(csvfile)
 
 
-# def test_graph1():
-#    csvfile = _run_cmdline(
-#        HERE / ".." / "figures" / "graphs_1.png"
-#    )
-#    _check_csv_file(csvfile)
+def test_graph1():
+    csvfile = _run_cmdline(
+        HERE / ".." / "figures" / "graphs_1.png",
+        [(1, 0), (6, 0), (0, 3)],
+        [(165, 52), (599, 51), (85, 151)],
+    )
+    _check_csv_file(csvfile)
 
 
-# def test_ecg():
-#    csvfile = _run_cmdline(
-#        HERE / ".." / "figures" / "ECGImage.png"
-#    )
-#    _check_csv_file(csvfile)
+def test_ecg():
+    csvfile = _run_cmdline(
+        HERE / ".." / "figures" / "ECGImage.png",
+        [(1, 0), (5, 0), (0, 1)],
+        [(290, 44), (1306, 43), (106, 301)],
+    )
+    _check_csv_file(csvfile)
 
 
 def test_grid():
