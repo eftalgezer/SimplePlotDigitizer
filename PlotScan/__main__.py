1c0f727f5a358a37ce457501ba8cf0ebc5c2e3b9
Eftal Gezer <eftalgezer@gmail.com>
2023-08-02 15:48:50 +0300

    Update __main__.py

--- PlotScan/__main__.py
+++ PlotScan/__main__.py
@@ -10,12 +10,17 @@ def main():
     """Main function"""
     description = """Digitize image."""
     parser = argparse.ArgumentParser(description=description)
-    parser.add_argument("INPUT", type=Path, help="Input image file.")
+    parser.add_argument(
+        "INPUT",
+        type=Path,
+        help="Path to the input image file.",
+    )
     parser.add_argument(
         "--pixel-tolerance",
         "-tol",
         required=False,
         default=1,
+        type=int,
         help="The maximum allowable difference in pixel coordinates. Default is 1.",
     )
     parser.add_argument(
@@ -24,9 +29,10 @@ def main():
         required=False,
         default=None,
         action="append",
-        help="Datapoints (min 3 required)."
-             " At least 3 points are recommended. e.g -p 0,0 -p 10,0 -p 0,1 "
-             "Make sure that point are comma separated without any space.",
+        help="Data points to define axes in the format x0,y0 -p x1,y1 -p x2,y2. "
+             "The first point defines the origin and at least 2 more points are "
+             "required to define the axes. "
+             "Make sure that points are comma-separated without any space.",
     )
     parser.add_argument(
         "--location",
@@ -34,8 +40,9 @@ def main():
         required=False,
         default=None,
         action="append",
-        help="Location of a points on figure in pixels (integer)."
-             " These values should appear in the same order as -p option.",
+        type=int,
+        help="Location of the points on the figure in pixels (integer). "
+             "These values should appear in the same order as the -p option.",
     )
     parser.add_argument(
         "--plot",
@@ -48,14 +55,14 @@ def main():
         "-o",
         required=False,
         type=str,
-        help="Name of the output file else trajectory will be written to "
+        help="Name of the output file. If not provided, the trajectory will be written to "
              " <INPUT>.traj.csv",
     )
     parser.add_argument(
         "--preprocess",
         required=False,
         action="store_true",
-        help="Preprocess the image. Useful with bad resolution images.",
+        help="Preprocess the image. Useful for improving bad resolution images.",
     )
     args = parser.parse_args()
     run(args)
