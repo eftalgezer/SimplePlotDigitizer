1bc20227d18d0ce44ac5395e59fa222b2df1d82e
Eftal Gezer <eftalgezer@gmail.com>
2024-08-05 02:28:22 +0300

    Update __init__.py

--- PlotScan/__init__.py
+++ PlotScan/__init__.py
@@ -1,5 +1,12 @@
-import logging
-import os
+"""
+A Python3 command line utility to digitize plots in batch mode.
+"""
 
-LOGLEVEL = os.environ.get("LOGLEVEL", "INFO").upper()
-logging.basicConfig(level=LOGLEVEL)
+from __future__ import absolute_import
+
+# meta
+__title__ = "PlotScan"
+__author__ = "Eftal Gezer"
+__license__ = "GNU GPL v3"
+__copyright__ = "Copyright 2023, Eftal Gezer"
+__version__ = "0.1.0"
